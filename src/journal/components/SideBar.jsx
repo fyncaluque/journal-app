ebe9629efbeb50435ad67713dc28bb165b80fc0e
Fernando M. Yncaluque <fyncaluque@gmail.com>
2023-01-02 18:49:07 -0500

    'Creacion de pages layouts views y componentes UI'

---
+++ src/journal/components/SideBar.jsx
@@ -0,0 +1,54 @@
+import { TurnedInNot } from '@mui/icons-material';
+import {
+  Box,
+  Divider,
+  Drawer,
+  Grid,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Toolbar,
+  Typography,
+} from '@mui/material';
+
+export const SideBar = ({ drawerWidth }) => {
+  return (
+    <Box
+      component="nav"
+      sx={{ width: { sm: drawerWidth }, flexShrink: 'sm:0' }}
+    >
+      <Drawer
+        variant="permanent"
+        open
+        sx={{
+          display: { sm: 'block' },
+          '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+        }}
+      >
+        <Toolbar>
+          <Typography variant="h6" noWrap component="div">
+            Fernando Dev
+          </Typography>
+        </Toolbar>
+        <Divider />
+        <List>
+          {['Enero', 'Febrero', 'Marzo', 'Abril'].map((text) => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  <TurnedInNot />
+                </ListItemIcon>
+                <Grid container>
+                  <ListItemText primary={text} />
+                  <ListItemText secondary={'Contenido de la tarea detallado'} />
+                </Grid>
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+    </Box>
+  );
+};
